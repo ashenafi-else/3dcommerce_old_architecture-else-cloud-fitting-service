b537227a19fedeee50e4bd37c00e81ce97984e1a
Maxim Cherchuk <maxim.cherchuk@gmail.com>
2017-04-12 13:28:30 +0300

    #13376 excess files removed

--- web/web/urls.py
+++ web/web/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from api.brands import *
-from api.user import *
-
 urlpatterns = [
-    url(r'', include('api.urls', namespace='api')),
     url(r'^admin/', admin.site.urls),
     url(r'^fitting/', include('fitting.urls')),
 ]
