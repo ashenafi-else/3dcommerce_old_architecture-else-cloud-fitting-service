f5acbb039fd1190c7345fa11c72571c8c8ec8149
Maxim Cherchuk <maxim.cherchuk@gmail.com>
2017-04-19 14:15:41 +0300

    Shoe divided into Last and Product

--- web/fitting/admin.py
+++ web/fitting/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from fitting.models import Scan, Shoe, User, Attribute, CompareResults
+from fitting.models import Scan, Last, Product, User, Attribute, CompareResults, Shoe
 
 # Register your models here.
 admin.site.register(Scan)
 admin.site.register(Shoe)
+admin.site.register(Last)
+admin.site.register(Product)
+# admin.site.register(Sizes)
 admin.site.register(User)
 admin.site.register(Attribute)
 admin.site.register(CompareResults)
