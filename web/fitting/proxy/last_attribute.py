f2dce528c19744d72737aca333d053ec025b791c
nsegen <n.segen@invento.by>
2017-05-03 13:01:34 +0300

    add last attribute proxy to admin

---
+++ web/fitting/proxy/last_attribute.py
@@ -0,0 +1,49 @@
+from django.contrib.admin.options import TabularInline
+from ..models import LastAttribute, Last, Size
+from .base_models_admin import BaseModelAdmin
+
+
+class LastAttributeProxy(LastAttribute):
+
+    class Meta:
+        proxy = True
+        verbose_name = 'Last Attribute'
+        verbose_name_plural = 'Last Attributes'
+
+
+class LastAttributeAdmin(BaseModelAdmin):
+
+    fields = [
+        'last',
+        'name',
+        'value',
+        'left_limit_value',
+        'best_value',
+        'right_limit_value',
+        'disabled',
+    ]
+
+    readonly_fields = [
+    ]
+
+    list_filter = [
+        'name',
+    ]
+
+    list_display = [
+        'last',
+        'value',
+        'left_limit_value',
+        'best_value',
+        'right_limit_value',
+        'disabled',
+    ]
+
+    list_editable = [
+        'value',
+        'left_limit_value',
+        'best_value',
+        'right_limit_value',
+        'disabled',
+    ]
+
