004739a95fd5dfb6e5a86dcb10b00889cf7e60f9
nsegen <n.segen@invento.by>
2017-09-05 16:37:56 +0300

    fix best size

--- web/fitting/views/best_size.py
+++ web/fitting/views/best_size.py
@@ -9,10 +9,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def compare_result_to_json(compare_result_left, compare_rsult_right):
+def compare_result_to_json(compare_result_left, compare_result_right):
 
     return {
-        'score': int((compare_result_left.compare_result + compare_rsult_right.compare_result) / 2),
+        'score': int((compare_result_left.compare_result + compare_result_right.compare_result) / 2),
         'output_model': compare_result_right.output_model,
         'output_model_3d': compare_result_right.output_model_3d,
         'size': compare_result_right.last.size.value,
